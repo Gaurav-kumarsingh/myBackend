ad8d23fe7d3ab89a46a054f8c449343ebb565763
Gaurav-kumarsingh <gk542998@gmail.com>
2026-04-23 02:01:06 +0530

    Port option changed

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require('express')
 // import express from 'express'
 const app = express()
 
-const port = 4000
+const port = process.env.PORT || 4000
 
 const githubData = {
   "login": "Gaurav-kumarsingh",
